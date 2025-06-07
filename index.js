a5841a28d3387c19d8273504f5187645316e844f
monishkajethanii <monishkayesim@gmail.com>
2025-06-07 14:08:45 +0530

    added swagger docs

--- index.js
+++ index.js
@@ -3,18 +3,31 @@ const router = require('./routes/router');
 const authCheck = require('./middleware/auth');
 const app = express();
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 app.use(cors({
   origin: "http://localhost:3000",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.js'], 
+};
+const specs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api',router)                          // api routes
 
 app.use(authCheck)                              // to add middleware
 app.use(express.json())                         // allow response body data
 app.use(express.urlencoded({extended:true}));   
-
-app.use('/api',router)                          // api routes
 
 app.listen(5000, ()=>{
     console.log('server started at port 5000')  // server running at 5000
--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,147 @@ const express = require("express");
 const router = express.Router();
 const blogController = require("../controller/blogController");
 const commentController = require("../controller/commentController")
-
+/**
+ * @swagger
+ * /api/blogs:
+ *   get:
+ *     summary: Get all blogs
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.get("/blogs", blogController.getBlogs);
+/**
+ * @swagger
+ * /addblog:
+ *   post:
+ *     summary: Create a new blog
+ *     tags: [Blogs]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *               author:
+ *                 type: string
+ *               tags:
+ *                  type: string
+ *             required:
+ *               - title
+ *               - content
+ *                 - author
+ *  
+ *     responses:
+ *       201:
+ *         description: Blog created successfully
+ *       400:
+ *         description: Bad request
+ */
 router.post("/addblog", blogController.addBlog);
+/**
+ * @swagger
+ * /editblog/{id}:
+ *   put:
+ *     summary: Update an existing blog
+ *     tags: [Blogs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the blog to update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *               author:
+ *                 type: string
+ *               tags:
+ *                 type: string
+ *             required:
+ *               - title
+ *               - content
+ *     responses:
+ *       200:
+ *         description: Blog updated successfully
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: Blog not found
+ */
+
 router.put("/editblog/:id", blogController.updateBlog);
+/**
+ * @swagger
+ * /deleteblog/{id}:
+ *   delete:
+ *     summary: Delete a blog by ID
+ *     tags: [Blogs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Blog ID to delete
+ *     responses:
+ *       200:
+ *         description: Blog deleted successfully
+ *       404:
+ *         description: Blog not found
+ *       500:
+ *         description: Internal server error
+ */
+
 router.delete("/deleteblog/:id", blogController.deleteBlog);
+/**
+ * @swagger
+ * /addcom/{id}:
+ *   post:
+ *     summary: Add a comment to a blog
+ *     tags: [Blogs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Blog ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               comments:
+ *                 type: string
+ *             required:
+ *               - comments
+ *     responses:
+ *       201:
+ *         description: Comment added successfully
+ *       400:
+ *         description: Comment text is required
+ *       404:
+ *         description: Blog not found
+ *       500:
+ *         description: Internal server error
+ */
+
 router.post("/addcom/:id",commentController.addComment)
 module.exports = router;
